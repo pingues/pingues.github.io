1d5900ec2cb54ec841754e0482b9aae8ae39cbec
Tarot de Marseille <mickael.badar@gmail.com>
2018-09-04 15:44:16 +0200

    Create ouiNon.js

---
+++ tarotv2/ouiNon.js
@@ -0,0 +1,164 @@
+/* OuiNon :
+ * 
+ *  0:non 
+ *  1:oui 
+ *  2:peut-être 
+ 
+ */
+
+var cartesOuiNon = [
+{
+	nom : "le mat",
+	ouiNon : 1,
+	image : "https://3.bp.blogspot.com/-RgEds9C5suU/WzJCADGIq1I/AAAAAAAAEbM/4-bsNJ9OKt0q-ePXvJpnR50jb_56hF5egCLcBGAs/s1600/0.jpg",
+	interpretation : "Le Mat se situe au tout d&eacute;but d&#039;un cycle, le premier; par cons&eacute;quent ses qualit&eacute;s sont l&#039;innocence, la fra&icirc;cheur, mais accompagn&eacute;es de pr&eacute;cipitation. Il se met en route, ne faisant aucun cas des conventions. C&#039;est un personnage libre, spontan&eacute; et joyeux. Lorsque la carte est plac&eacute;e &agrave; la fin du jeu, de m&ecirc;me que parfois en d&eacute;but de jeu, elle peut avoir une signification tout autre; elle repr&eacute;sente alors un personnage incapable de faire face &agrave; la r&eacute;alit&eacute; et qui s&#039;enferme dans son propre monde, totalement herm&eacute;tique aux autres.<br /> "
+},
+
+{
+	nom : "le bateleur",
+	ouiNon : 1,
+	image : "https://1.bp.blogspot.com/-7u4dl5eSdfE/WzJC_omwJYI/AAAAAAAAEbY/66Rkalsp4lYa17Gci2WyTs0LlqfUeM_mACLcBGAs/s1600/1.jpg",
+	interpretation : "Le Bateleur repr&eacute;sente celui qui tient entre ses mains les instruments permettant de changer le cours des &eacute;v&eacute;nements selon son bon vouloir, ce que lui permet sa vive intelligence. <br /> Le Bateleur sait parfaitement adapter les moyens aux fins et parvient ainsi &agrave; son but. Il symbolise par ailleurs le changement, le mouvement, le dynamisme, la cr&eacute;ativit&eacute;, la transformation et la volont&eacute;. "
+},
+
+{
+	nom : "la papesse",
+	ouiNon : 2,
+	image : "https://3.bp.blogspot.com/-GhIDRMmfIMA/WzJM8n78nMI/AAAAAAAAEb4/1plwqnBMApMItXNJZfRm8B9TieGhlLXEACLcBGAs/s1600/2.jpg",
+	interpretation : "La Papesse est en mesure de d&eacute;couvrir avec une grande facilit&eacute; les aspects les plus importants, occultes et profonds, que peuvent nous r&eacute;v&eacute;ler les choses, les personnes et les &eacute;v&eacute;nements. C&#039;est un personnage qui se sert constamment de son intuition, cherche le fond des choses derri&egrave;re les apparences et d&eacute;couvre le changement qui annonce le mouvement des choses. Elle pr&eacute;f&egrave;re l&#039;activit&eacute; au repos et sera attir&eacute;e par l&#039;occulte et le myst&eacute;rieux ainsi que par le mouvement et le silence."
+},
+
+{
+	nom : "l&#039;imp&eacute;ratrice",
+	ouiNon : 1,
+	image : "https://3.bp.blogspot.com/-UWtnGtBNPnc/WzOS5ne25II/AAAAAAAAFms/VA3xKDW5aRkjmfVed2JeZuMeEk9sxeLUQCLcBGAs/s1600/3.jpg",
+	interpretation : "Ma&icirc;trise et confiance en soi conduisant &agrave; l&#039;abondance mat&eacute;rielle et spirituelle. Grande intelligence se traduisant par des succ&egrave;s mat&eacute;riels. Capacit&eacute; productive, f&eacute;condit&eacute;, maternit&eacute; et claire image de l&#039;influence f&eacute;minine et des attributs f&eacute;minins. <br /> La t&acirc;che de l&#039;Imp&eacute;ratrice porte ses fruits et cro&icirc;t de sorte qu&#039;elle cr&eacute;e des richesses en chemin et encourage &agrave; la pers&eacute;v&eacute;rance. Si la carte est tir&eacute;e &agrave; l&#039;envers, les attributs maternels se convertissent en une attitude trop protectrice qui peut &ecirc;tre castratrice ou symboliser la st&eacute;rilit&eacute;. " 
+},
+
+{
+	nom : "l&#039;empereur",
+	ouiNon : 2,
+	image : "https://4.bp.blogspot.com/-OG3xbhKzVFc/WzOXxlfms6I/AAAAAAAAFnE/_1ays5yWv5EydqUDPuIUCUk6kKGCiBv0ACLcBGAs/s1600/4.jpg",
+	interpretation : "L&#039;&eacute;nergie propre au personnage le rend tr&egrave;s pers&eacute;v&eacute;rant, lui donne une grande r&eacute;sistance &agrave; la fatigue, &agrave; la n&eacute;gligence, &agrave; l&#039;indiff&eacute;rence. Puissant protecteur, il peut &ecirc;tre rattach&eacute; &agrave; l&#039;image et &agrave; l&#039;influence masculines. Epoux, p&egrave;re qui veillera sur ses enfants. Grande facilit&eacute; pour obtenir biens, richesse et dons. Son pouvoir s&#039;affirme au fur et &agrave; mesure qu&#039;il consolide sa position avantageuse."
+},
+
+{
+	nom : "le pape",
+	ouiNon : 2,
+	image : "https://4.bp.blogspot.com/-1w1EXHRgsVA/WzOdhvPkgCI/AAAAAAAAFnk/v82Uu0CQt6sSpZlSLjYJEZ7UMfKrk5BCQCLcBGAs/s1600/5.jpg",
+	interpretation : "Symbole de la stabilit&eacute; et des choses immuables. Ce personnage se r&eacute;alise dans l&#039;enseignement car il poss&egrave;de de grandes qualit&eacute;s &eacute;ducatives. Il aime partager ses connaissances et le fait avec ordre, m&eacute;thode et grande clart&eacute;. "
+},
+
+{
+	nom : "l&#039;amoureux",
+	ouiNon : 1,
+	image : "https://4.bp.blogspot.com/-_RFffTVUsEw/WzOiUYJnEvI/AAAAAAAAFn8/SCfAkvIWS8o6gjD18zOmiuNEHdUThupvgCLcBGAs/s1600/6.jpg",
+	interpretation : "Le personnage se situe &agrave; un moment o&ugrave; il a pris une d&eacute;cision entre deux choix oppos&eacute;s. Il faut qu&#039;il le manifeste sans tarder afin que la possibilit&eacute; contraire ne constitue pas un fardeau et un obstacle &agrave; ses projets. Il faut &eacute;couter la voix du sentiment et &eacute;loigner les directives de la raison, parce que le chemin du coeur signifie aventure, myst&egrave;re et une fin g&eacute;n&eacute;ralement impr&eacute;visible. L&#039;objet de ce d&eacute;sir ardent constitue une fin imminente. La richesse qu&#039;ouvrira ce choix sera incalculable. Y renoncer peut mener &agrave; une stagnation st&eacute;rile."
+},
+
+{
+	nom : "le chariot",
+	ouiNon : 1,
+	image : "https://4.bp.blogspot.com/-G5EIYPjAEX4/WzO0o5yIrPI/AAAAAAAAFoU/GT8_lh3syQ4kwcsjbSM9b7YySvQsfLV5wCLcBGAs/s1600/7.jpg",
+	interpretation : "Cette carte indique la volont&eacute; in&eacute;branlable d&#039;aboutir, volont&eacute; qui ne c&eacute;dera devant aucun obstacle, quel qu&#039;il soit. La constance viendra &agrave; bout des difficult&eacute;s.<br /> Le personnage qui m&egrave;ne le Chariot dispose du moyen le plus s&ucirc;r pour aboutir &agrave; ses fins, &agrave; savoir une volont&eacute; de fer. Il laisse derri&egrave;re lui les obstacles, l&eacute;gers contretemps qui ne semblent gu&egrave;re le freiner dans sa foul&eacute;e et qu&#039;il parvient &agrave; surmonter ais&eacute;ment. "
+},
+
+{
+	nom : "la justice",
+	ouiNon : 2,
+	image : "https://4.bp.blogspot.com/-536ysq9SB78/WzSnka8kcmI/AAAAAAAAFpw/vv55waiZoaEj_KJMb8dzFbw99nZiCigDgCLcBGAs/s1600/8.jpg",
+	interpretation : "La balance maintient un &eacute;quilibre parfait qui tend vers une harmonie naturelle et c&#039;est l&agrave; sa premi&egrave;re signification. La Justice apporte l&#039;harmonie &agrave; tout ce qui l&#039;entoure, elle sait adoucir les temp&eacute;raments, apaiser les situations violentes. Sous sa houlette, les discussions peuvent se poursuivre dans le calme et la s&eacute;r&eacute;nit&eacute;. "
+},
+
+{
+	nom : "l&#039;hermite",
+	ouiNon : 2,
+	image : "https://3.bp.blogspot.com/-znVFhJ6x4uE/WzSgPbanAlI/AAAAAAAAFpE/H-dNHF1NU_AOqXBqyTOpeVAEycnBoWAdACLcBGAs/s1600/9.jpg",
+	interpretation : "Le personnage a besoin de s&#039;arr&ecirc;ter pour r&eacute;fl&eacute;chir un moment sur lui-m&ecirc;me. Mais il ne peut demeurer inactif et doit, au contraire, faire appel &agrave; toutes ses connaissances pour savoir quel chemin suivre. Cette carte indique une &eacute;tape du cheminement personnel o&ugrave; l&#039;on d&eacute;couvre que rien n&#039;est d&eacute;finitif mais que la vie continue et que, peu &agrave; peu, on parvient &agrave; mieux la conna&icirc;tre et la comprendre."
+},
+
+{
+	nom : "la roue de la fortune",
+	ouiNon : 1,
+	image : "https://2.bp.blogspot.com/-xPz0v4Yb4Sk/WzSjZmNb_TI/AAAAAAAAFpY/Kn5-fiPSO04FwWPO5nulai9-qFcSwnHRgCLcBGAs/s1600/10.jpg",
+	interpretation : "La Roue de Fortune ne cesse de tourner et dans son mouvement elle peut apporter mille et une possibilit&eacute;s dont on peut tirer le plus grand parti. Les situations s&#039;encha&icirc;nent les unes aux autres; l&#039;on finit par r&eacute;colter ce que l&#039;on a sem&eacute;, si on ne laisse pas &eacute;chapper les occasions. Le changement soudain de circonstances, les situations nouvelles et impr&eacute;vues favoriseront un &eacute;change d&#039;id&eacute;es et de relations tr&egrave;s vari&eacute;es. "
+},
+
+{
+	nom : "la force",
+	ouiNon : 1,
+	image : "https://2.bp.blogspot.com/-OF38DKdDIvk/WzScPu_NBkI/AAAAAAAAFos/rQGay-RfP20tvNVGAtuohtxGl5w-3GbVwCLcBGAs/s1600/11.jpg",
+	interpretation : "La carte nous propose ici un personnage qui, sans difficult&eacute;s, parvient &agrave; ma&icirc;triser de ses mains et de son corps le symbole de la force brute. L&#039;intelligence contr&ocirc;le et domine ici les passions animales, repr&eacute;sent&eacute;es par le lion. C&#039;est l&#039;image de la force soumise et utilis&eacute;e &agrave; bon escient, la canalisation des instincts et des passions par la volont&eacute; et l&#039;intelligence. C&#039;est aussi la ma&icirc;trise des circonstances. Le lien avec la force physique est &eacute;vident, mais le personnage demeure ma&icirc;tre de sa force et sait l&#039;utiliser. "
+},
+
+{
+	nom : "le pendu",
+	ouiNon : 2,
+	image : "https://4.bp.blogspot.com/-vJwvhYZooiw/WzSq5bsz7VI/AAAAAAAAEcc/1X9xosDBaFIL0Bq4GG28KE7Snll6g65YQCLcBGAs/s1600/12.jpg",
+	interpretation : "Il s&#039;agit ici d&#039;un personnage qui cherche &agrave; d&eacute;passer l&#039;aspect superficiel des choses et &agrave; d&eacute;couvrir leur sens intrins&egrave;que. Le personnage se caract&eacute;rise par son agilit&eacute; mentale, mais il a les pieds li&eacute;s et ne peut mener &agrave; bien les projets qu&#039;il con&ccedil;oit ainsi. Il y a impuissance r&eacute;alisatrice et impossibilit&eacute; de mise en pratique. Le personnage repr&eacute;sent&eacute; n&#039;est pas libre, mais ne cherche pas non plus &agrave; se d&eacute;faire de ses liens. Il pr&eacute;f&egrave;re r&eacute;fl&eacute;chir sur sa situation et consid&eacute;rer la route parcourue. "
+},
+
+{
+	nom : "l&#039;arcane sans nom",
+	ouiNon : 0,
+	image : "https://4.bp.blogspot.com/-uMH7TFu84MM/WzSuAyB66FI/AAAAAAAAEc0/qL6JI9lqatA2RYKJsYy_Q4KI39avtWWGACLcBGAs/s1600/13.jpg",
+	interpretation : "Cette carte repr&eacute;sente la fin d&#039;une situation, d&#039;une oeuvre, sur laquelle il ne sera cependant plus possible de revenir. La Mort avance et fauche tout ce qu&#039;elle trouve sur son passage et qui est parvenu &agrave; une fin. C&#039;est pourquoi son influence est purifiante puisqu&#039;elle met fin &agrave; ce qui n&#039;&eacute;tait plus en mesure d&#039;&ecirc;tre utilis&eacute; et permet le renouvellement sous tous ses aspects. Apr&egrave;s son passage, la terre peut &agrave; nouveau &ecirc;tre fertilis&eacute;e. La Mort d&eacute;livre le personnage du masque derri&egrave;re lequel il s&#039;est jusqu&#039;alors cach&eacute;, afin qu&#039;il puisse se r&eacute;v&eacute;ler tel qu&#039;il est et donner libre cours &agrave; ses instincts r&eacute;novateurs. "
+},
+
+{
+	nom : "la temp&eacute;rance",
+	ouiNon : 1,
+	image : "https://4.bp.blogspot.com/-Oz7ghI3OgSo/WzTLUL4mXSI/AAAAAAAAEdM/xp6UA--pm9keFK2ur2SAyKmewMyo6pbRgCLcBGAs/s1600/14.jpg",
+	interpretation : "L&#039;image fait r&eacute;f&eacute;rence au cadre quotidien. aux t&acirc;ches r&eacute;p&eacute;titives. Elle symbolise la capacit&eacute; de modifier les choses en fonction de leur utilisation afin de parvenir &agrave; un &eacute;quilibre entre la pratique et l&#039;usage utile et n&eacute;cessaire. L&#039;image exprime donc la flexibilit&eacute; et la richesse que l&#039;on peut trouver dans des rapports qui se pr&eacute;sentent de fa&ccedil;on quotidienne. "
+},
+
+{
+	nom : "le diable",
+	ouiNon : 0,
+	image : "https://4.bp.blogspot.com/-WqOfSLttM9M/WzjTzlnELtI/AAAAAAAAEe0/Iv91DRFJxR8VfD4uUdCCmdO-t-K_-LjCQCLcBGAs/s1600/15.jpg",
+	interpretation : "La magie noire est une force obscure qui fa&ccedil;onne ses oeuvres &agrave; partir des aspects les plus secrets de l&#039;esprit. Sa force s&#039;exprime de fa&ccedil;on non contr&ocirc;l&eacute;e, extravagante, donnant parfois naissance &agrave; des oeuvres surprenantes poss&eacute;dant la beaut&eacute; du Diable. Mais ces oeuvres se produisent dans le d&eacute;sordre et la confusion. Cette absence de tout contr&ocirc;le sur les passions m&egrave;ne in&eacute;luctablement au chaos et &agrave; l&#039;incoh&eacute;rence. "
+},
+
+{
+	nom : "la maison dieu",
+	ouiNon : 0,
+	image : "https://2.bp.blogspot.com/-TrOMsY44DBQ/WzeL2wHQHnI/AAAAAAAAFrM/edkn8knawQEyYSVOq1GMVjCq-8FSHBRtwCEwYBhgL/s1600/16.jpg",
+	interpretation : "La partie sup&eacute;rieure d&#039;un b&acirc;timent d&#039;apparence tr&egrave;s solide s&#039;&eacute;croule et entra&icirc;ne dans sa chute hommes et objets. La base demeure cependant solidement ancr&eacute;e au sol. Ceci annonce qu&#039;une entreprise qui avait d&eacute;marr&eacute; sur des bases solides risque d&#039;&ecirc;tre fortement compromise. Ce qui semblait stable et durable est abattu par la foudre fatale et mortelle. Ce qui reste debout n&#039;est plus d&#039;aucune utilit&eacute;. La foudre s&#039;est charg&eacute;e d&#039;&eacute;liminer quelque chose qui pouvait &ecirc;tre interpr&eacute;t&eacute; comme un obstacle; le consultant peut d&#039;abord s&#039;identifier &agrave; la foudre avant de se comparer &agrave; la Tour d&eacute;truite. "
+},
+
+{
+	nom : "l&#039;&eacute;toile",
+	ouiNon : 1,
+	image : "https://1.bp.blogspot.com/-FLbvjXmeNlU/WzYABjn6r6I/AAAAAAAAEdo/3M7VvQPCZ-0pupfItIHG6tSdMK-GQpUHwCLcBGAs/s1600/17.jpg",
+	interpretation : "L&#039;Etoile verse &agrave; deux mains tous les attributs qu&#039;elle porte en elle afin de les incorporer &agrave; la vie et de r&eacute;partir la sympathie, l&#039;affection, la franchise, la loyaut&eacute;, la joie et la satisfaction de par le monde. L&#039;Etoile irradie la beaut&eacute;, la sensibilit&eacute;, l&#039;intuition et le sentiment artistique. Elle &eacute;voque tout ce qui apporte lumi&egrave;re et beaut&eacute; par l&#039;interm&eacute;diaire d&#039;un moyen d&#039;expression et de r&eacute;alisation appropri&eacute;. L&#039;Etoile repr&eacute;sente un personnage d&#039;une grande sensibilit&eacute;; tout ce qu&#039;elle touche est transform&eacute; par sa beaut&eacute; et son aura."
+},
+
+{
+	nom : "la lune",
+	ouiNon : 0,
+	image : "https://4.bp.blogspot.com/-EqnFB1os7u0/WzZCUGlnG-I/AAAAAAAAEeA/TqWneuK2oXYv8b62Im3MTCDS6Kqp5F_fACLcBGAs/s1600/18.jpg",
+	interpretation : "La Lune est un monde occulte qui appartient au subconscient et qui filtre toutes les pens&eacute;es les plus secr&egrave;tes que peut dissimuler toute personne. Toutes les pens&eacute;es qui &eacute;voluent dans les replis les plus profonds de l&#039;esprit sont r&eacute;gies par la Lune. Un voyage dans les profondeurs occultes peut donc apporter r&eacute;v&eacute;lations &eacute;tonnantes et enrichissement."
+},
+
+{
+	nom : "le soleil",
+	ouiNon : 1,
+	image : "https://2.bp.blogspot.com/-E9jzg4_D3eo/WzZQWD3auUI/AAAAAAAAEeg/pVURmRENSnUJeXj9ZB1mz6eb_vXbCJu2ACLcBGAs/s1600/19.jpg",
+	interpretation : "Le Soleil est l&#039;expression d&#039;une intelligence claire et d&#039;une sagesse radieuse qui se manifestent sous les aspects les plus brillants. C&rsquo;est l&rsquo;astre qui donne la vie, qui favorise son d&eacute;veloppement et qui p&eacute;n&egrave;tre au fond des choses pour qu&rsquo;elles croissent et prennent vigueur.<br /> Le Soleil est vitalit&eacute;, &eacute;nergie, cr&eacute;ativit&eacute; et spontan&eacute;it&eacute;. Il illustre une radiation b&eacute;n&eacute;fique et le triomphe. Il symbolise la pl&eacute;nitude et la satisfaction."
+},
+
+{
+	nom : "le jugement",
+	ouiNon : 1,
+	image : "https://2.bp.blogspot.com/-jdumG6hzR2o/WzeFzXfAtTI/AAAAAAAAFqk/wmPPqsqKescco3FUfiyw706XS-DDtYM2gCLcBGAs/s1600/20.jpg",
+	interpretation : "Cette carte confronte le consultant avec l&rsquo;id&eacute;e que les autres se sont forg&eacute;s de lui. En ce point du voyage, presque au bout du chemin, le Jugement est une &eacute;tape d&eacute;finitive. Il n&rsquo;y a d&rsquo;autre solution que de clarifier les choses afin de mener a bien la mission &agrave; accomplir, apr&egrave;s une m&ucirc;re r&eacute;flexion sur la route parcourue. Il faut alors faire preuve d&rsquo;une grande honn&ecirc;tete vis-a-vis de soi-m&ecirc;me et d&rsquo;un grand sens de la responsabilit&eacute;; de cesdeux facteurs d&eacute;pendra la reussite ou l&#039;echec.<br /> Le sujet est nu afin de laisser d&eacute;couvrir ses secrets."
+},
+
+{
+	nom : "le monde",
+	ouiNon : 1,
+	image : "https://3.bp.blogspot.com/-BEu_gslGjkE/WzeIkXUU2vI/AAAAAAAAFq4/YAMLF_kl9oQ1RVvwNbHzpxue5efuGVF3gCLcBGAs/s1600/21.jpg",
+	interpretation : "Le Monde qui se d&eacute;ploie devant nous, dans toute sa richesse et sa splendeur, contient tous les &eacute;l&eacute;ments et les moyens permettant une cr&eacute;ation constante et des r&eacute;alisations positives. Il symbolise la r&eacute;alisation personnelle dans un domaine vaste et empli de possibilit&eacute;s diverses.<br /> Il exprime la croissance, le d&eacute;veloppement de tout ce qui vit et la manifestation de la vie sous toutes ses formes."
+}
+
+];
