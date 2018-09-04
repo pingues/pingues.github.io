8e79de3e864d224c8c7fdad09ee7a0149d9a4249
Tarot de Marseille <mickael.badar@gmail.com>
2018-09-04 15:41:33 +0200

    Create resoudOuiNon.js

---
+++ tarotv2/resoudOuiNon.js
@@ -0,0 +1,36 @@
+		/* Genere un nombre entre min inclus et max inclus */
+		
+		function entierAleatoire(min, max){
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+		
+		
+		function rendReponseOuiNon(carte){
+			if (cartesOuiNon[carte]["ouiNon"] == 0){
+				return "non";
+			}
+			else if (cartesOuiNon[carte]["ouiNon"] == 1){
+				return "oui";
+			}
+			else if (cartesOuiNon[carte]["ouiNon"] == 2){
+				return "peut-&ecirc;tre";
+			}
+		}
+		
+	    function resoudOuiNon(){
+			var numCarte = entierAleatoire(0, 21);
+			var reponseOuiNon = rendReponseOuiNon (numCarte);
+			
+			var resultat = "<h2>" + cartesOuiNon[numCarte]["nom"] 
+				+ "</h2><img src=\"" + cartesOuiNon[numCarte]["image"] + "\"><br>"
+				+ "La r&eacute;ponse est : " + reponseOuiNon + "<br>"
+				+ cartesOuiNon[numCarte]["interpretation"];
+
+			$("#reponse").toggle( "slow", function(){
+				$("#reponse").replaceWith('<div id="reponse">' + resultat + '</div>');
+				$("#reponse").fadeIn( "slow" );
+			});
+			
+			//$("#reponse").replaceWith('<div id="reponse">' + resultat + '</div>');
+
+		}
