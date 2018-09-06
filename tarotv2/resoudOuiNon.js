7853ffa79c4a53b604a06683466bb356fd340c43
Tarot de Marseille <mickael.badar@gmail.com>
2018-09-06 13:50:28 +0200

    Update resoudOuiNon.js

--- tarotv2/resoudOuiNon.js
+++ tarotv2/resoudOuiNon.js
@@ -20,17 +20,20 @@
 	    function resoudOuiNon(){
 		var numCarte = entierAleatoire(0, 21);
 		var reponseOuiNon = rendReponseOuiNon (numCarte);
-			
-			/*var resultat = "<h2>" + cartesOuiNon[numCarte]["nom"] 
-				+ "</h2><img class=\"carte\" src=\"" + cartesOuiNon[numCarte]["image"] + "\"><br>"
-				+ "La r&eacute;ponse est : " + reponseOuiNon + "<br>"
-				+ cartesOuiNon[numCarte]["interpretation"];
-*/
+		var bouton = "<center>"+
+			"<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.reload();\">"+
+   				"<i class=\"fa fa-repeat\" aria-hidden=\"true\"></i>"+
+   				"Refaire un tirage"+
+			"</button>"+
+			"</center>";
+
+
 		var resultat = "<h2>Interprétation du tirage</h2>"
 			+ "<img class=\"carte\" src=\"" + cartesOuiNon[numCarte]["image"] + "\"><br>"
 			+ "<span style=\"text-transform:capitalize; font-size:150%\">" + cartesOuiNon[numCarte]["nom"] 
 			+ "</span> <span style=\"font-size:150%\">: la r&eacute;ponse est <strong>" + reponseOuiNon + "</strong></span><br>"
-			+ cartesOuiNon[numCarte]["interpretation"];    
+			+ cartesOuiNon[numCarte]["interpretation"]
+			+ "<br><br>" + bouton;    
 		    
 		$("#reponse").toggle( "slow", function(){
 			$("#reponse").replaceWith('<div id="reponse">' + resultat + '</div>');
