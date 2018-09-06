720354a10294747147ed23275723560fb3229767
Tarot de Marseille <mickael.badar@gmail.com>
2018-09-06 18:20:44 +0200

    Update resoudOuiNon.js

--- tarotv2/resoudOuiNon.js
+++ tarotv2/resoudOuiNon.js
@@ -28,7 +28,7 @@
 			"</center>";
 
 		var pub ="<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"+
-			"<ins class=\"adsbygoogle\"+
+			"<ins class=\"adsbygoogle\""+
      			"style=\"display:inline-block;width:728px;height:90px\"+
      			"data-ad-client=\"ca-pub-5872858147264773\"+
      			"data-ad-slot=\"3077370541\"></ins>"+
