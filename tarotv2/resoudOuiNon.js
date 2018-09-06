3d036e09669ec6f0b095964c74c138fc6d0dce39
Tarot de Marseille <mickael.badar@gmail.com>
2018-09-06 18:18:57 +0200

    Update resoudOuiNon.js

--- tarotv2/resoudOuiNon.js
+++ tarotv2/resoudOuiNon.js
@@ -27,9 +27,18 @@
 			"</button>"+
 			"</center>";
 
+		var pub ="<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"+
+			"<ins class=\"adsbygoogle\"+
+     			"style=\"display:inline-block;width:728px;height:90px\"+
+     			"data-ad-client=\"ca-pub-5872858147264773\"+
+     			"data-ad-slot=\"3077370541\"></ins>"+
+			"<script>"+
+     			"	(adsbygoogle = window.adsbygoogle || []).push({});"+
+			"</script>";
 
 		var resultat = "<h2>Interprétation du tirage</h2>"
-			+ "<br><img class=\"img-responsive\" src=\"" + cartesOuiNon[numCarte]["image"] + "\"><br>"
+			+ "<br><img class=\"img-responsive\" src=\"" + cartesOuiNon[numCarte]["image"] + "\"><br><br>"
+			+ pub + "<br><br>"
 			+ "<span style=\"text-transform:capitalize; font-size:150%\">" + cartesOuiNon[numCarte]["nom"] 
 			+ "</span> <span style=\"font-size:150%\">: la r&eacute;ponse est <strong>" + reponseOuiNon + "</strong></span><br>"
 			+ cartesOuiNon[numCarte]["interpretation"]
